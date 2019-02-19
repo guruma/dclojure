fe3678c8e6c0b317770df91c4a934fd15c630383
Sang-Kyu Park <psk810@gmail.com>
2019-02-19 19:09:13 +0900

    test_file

--- source/app.d
+++ source/app.d
@@ -1,4 +1,13 @@
-import std.stdio, std.process;
+import 	std.stdio, 
+	std.process,
+	std.string, 
+	std.path, 
+	std.process, 
+	std.algorithm;
+	core.sys.posix.sys.stat;
+
+import std.stdio, std.string, std.path, std.process, std.algorithm;
+
 
 void main()
 {
@@ -10,4 +19,35 @@ void main()
 
     auto pwd = environment.get("PWD");
     writeln("PWD = ", pwd);
+
+    string paths = environment.get("PATH");
+
+    foreach (path; paths.split(pathSeparator).map!(path => asAbsolutePath(path))) {
+      writeln(path);
+    }
+
+    if(test_file('e', "dclojure")) writeln("exist");
+}
+
+bool test_file(char b, string fname)
+{
+    stat_t st;
+    int ret = stat(cast(char*)fname, &st);
+
+    if( ret != 0 ) return false;
+
+    switch(b) {
+    case 'e':
+	return (ret == 0);
+    case 'f':
+        return (0 != (st.st_mode & S_IFREG));
+    case 'x':
+        return (0 != (st.st_mode & S_IXUSR));
+    case 'd':
+        return (0 != (st.st_mode & S_IFDIR));
+    default:
+	break;
+    }
+
+    return false;
 }
