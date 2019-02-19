232bf961abf6748da6e83eb98c8a8b4d211a3fc8
Sang-Kyu Park <psk810@gmail.com>
2019-02-19 19:22:18 +0900

    Modify test_file function

--- source/app.d
+++ source/app.d
@@ -29,24 +29,26 @@ void main()
     if(test_file('e', "dclojure")) writeln("exist");
 }
 
-bool test_file(char b, string fname)
+bool test_file(char opt, string fname)
 {
     stat_t st;
-    int ret = stat(cast(char*)fname, &st);
+    bool is_exist = (0 == stat(cast(char*)fname, &st));
 
-    if( ret != 0 ) return false;
+    if('e' == opt) {
+      return is_exist;
+    }
 
-    switch(b) {
-    case 'e':
-	return (ret == 0);
-    case 'f':
-        return (0 != (st.st_mode & S_IFREG));
-    case 'x':
-        return (0 != (st.st_mode & S_IXUSR));
-    case 'd':
-        return (0 != (st.st_mode & S_IFDIR));
-    default:
-	break;
+    if (is_exist) {
+	switch(opt) {
+	case 'f':
+	    return (0 != (st.st_mode & S_IFREG));
+	case 'x':
+	    return (0 != (st.st_mode & S_IXUSR));
+	case 'd':
+	    return (0 != (st.st_mode & S_IFDIR));
+	default:
+	    break;
+	}
     }
 
     return false;
