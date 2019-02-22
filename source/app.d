135ec6b87a7c3e67bdcadc8ee4003cb620e6f9ae
Sang-Kyu Park <psk810@gmail.com>
2019-02-22 16:36:35 +0900

    Modify .gitignore

--- source/app.d
+++ source/app.d
@@ -1,29 +1,9 @@
-import  std.stdio, 
-        std.string, 
-        std.path, 
-        std.process,
-        std.algorithm,
-        core.sys.posix.sys.stat;
-
+import  std.stdio,
+        std.exception,
+        dclojure.file;
 
 void main()
 {
-    auto home = environment.get("HOME");
-    writeln("HOME = ", home); 
-
-    auto javaHome = environment.get("JAVA_HOME");
-    writeln("JAVA_HOME = ", javaHome);
-
-    auto pwd = environment.get("PWD");
-    writeln("PWD = ", pwd);
-
-    string paths = environment.get("PATH");
-
-    foreach (path; paths.split(pathSeparator).map!(path => asAbsolutePath(path)))
-    {
-        writeln(path);
-    }
-
     if(test_file('e', "dclojure")) writeln("exist");
  
     runJava("/usr/bin/java -version");
@@ -81,4 +61,3 @@ bool test_file(char opt, string fname)
 
     return false;
 }
-
