d2a0bcba9e2c079ab66e631d5b3d973c1aca6c7f
Sang-Kyu Park <psk810@gmail.com>
2019-02-22 16:47:54 +0900

    Apply dclojure.file module.

--- source/app.d
+++ source/app.d
@@ -1,10 +1,31 @@
-import  std.stdio,
-        std.exception,
+import  std.stdio, 
+        std.string, 
+        std.path, 
+        std.process,
+        std.algorithm,
+        core.sys.posix.sys.stat,
         dclojure.file;
+
 
 void main()
 {
-    if(test_file('e', "dclojure")) writeln("exist");
+    auto home = environment.get("HOME");
+    writeln("HOME = ", home); 
+
+    auto javaHome = environment.get("JAVA_HOME");
+    writeln("JAVA_HOME = ", javaHome);
+
+    auto pwd = environment.get("PWD");
+    writeln("PWD = ", pwd);
+
+    string paths = environment.get("PATH");
+
+    foreach (path; paths.split(pathSeparator).map!(path => asAbsolutePath(path)))
+    {
+        writeln(path);
+    }
+
+    if("dclojure".exists) writeln("exist");
  
     runJava("/usr/bin/java -version");
     writeln("configDir = ", configDir());
@@ -32,32 +53,4 @@ string configDir()
         return buildPath(dir, ".clojure");
     else
         return dir;
-}
-
-bool test_file(char opt, string fname)
-{
-    stat_t st;
-    bool is_exist = (0 == stat(cast(char*)fname, &st));
-
-    if('e' == opt)
-    {
-        return is_exist;
-    }
-
-    if (is_exist) 
-    {
-        switch(opt) 
-        {
-            case 'f':
-                return (0 != (st.st_mode & S_IFREG));
-            case 'x':
-                return (0 != (st.st_mode & S_IXUSR));
-            case 'd':
-                return (0 != (st.st_mode & S_IFDIR));
-            default:
-                break;
-        }
-    }
-
-    return false;
 }
