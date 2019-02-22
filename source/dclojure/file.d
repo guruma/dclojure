3ed43b9bd49ec1376807666b8b2955015074e0be
Sang-Kyu Park <psk810@gmail.com>
2019-02-22 16:29:16 +0900

    Add dclojure.file Module.

---
+++ source/dclojure/file.d
@@ -0,0 +1,59 @@
+module dclojure.file;
+
+import  std.stdio,
+        std.range.primitives,
+        std.traits;
+
+bool exists(R)(R name)
+{
+    static import std.file;
+    return std.file.exists(name);
+}
+
+@property bool isDir(R)(R name)
+if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) && !isConvertibleToString!R)
+{
+    static import std.file;
+    return std.file.isDir(name);
+}
+
+@property bool isFile(R)(R name)
+{
+    static import std.file;
+    return std.file.isFile(name);
+}
+
+bool isExec(string fname)
+{
+    bool isExist = false;
+
+    version (Windows)
+    {
+        import core.sys.windows.sys.stat;
+        struct_stat st;
+
+        isExist = (0 == stat(cast(char*)fname, &st));
+
+        if( ! isExist ) 
+            return false;
+
+        return (0 != (st.st_mode & S_IEXEC));
+    }
+    else version (Posix)
+    {
+        import core.sys.posix.sys.stat;
+        stat_t st;
+
+        isExist = (0 == stat(cast(char*)fname, &st));
+
+        if( ! isExist ) 
+            return false;
+
+        return (0 != (st.st_mode & S_IXUSR));
+    }
+}
+
+unittest 
+{
+    assert(0 == 0);
+}
