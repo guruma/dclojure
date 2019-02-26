1d45c485c9ba5f09506dd2a5f77af24400cdbdc2
Sang-Kyu Park <psk810@gmail.com>
2019-02-26 14:06:14 +0900

    ...

--- source/dclojure/file.d
+++ source/dclojure/file.d
@@ -7,7 +7,6 @@ import std.string,
        std.range.primitives;
 
 static import std.file;
-import std.stdio;
 
 
 private bool _isExec(R)(R name)
@@ -16,22 +15,22 @@ if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
 {
     version (Windows)
     {
-    	import core.sys.windows.stat;
+        import core.sys.windows.stat;
 
         static auto trustedStat(const(char)* namez, ref struct_stat st) @trusted
         {
-	        return stat(namez, &st);
+            return stat(namez, &st);
         }
 
-	struct_stat st = void;
+        struct_stat st = void;
         immutable result = trustedStat(name.toStringz(), st);
 
         return (0 != (result & S_IEXEC));
-	//return (result & S_IFMT) == S_IEXEC;
+        //return (result & S_IFMT) == S_IEXEC;
     }
     else version (Posix)
     {
-	import core.sys.posix.sys.stat;
+        import core.sys.posix.sys.stat;
 
         static auto trustedStat(const(char)* namez, ref stat_t buf) @trusted
         {
@@ -50,11 +49,11 @@ public bool isExec(string name)
 {
     try 
     {
-	return _isExec(name);
+        return _isExec(name);
     } 
     catch(Exception e)
     {
-	return false;
+        return false;
     }
 }
 
@@ -62,11 +61,11 @@ public bool exists(string name)
 {
     try 
     {
-	return std.file.exists(name);
+        return std.file.exists(name);
     } 
     catch(Exception e)
     {
-	return false;
+        return false;
     }
 }
 
@@ -74,11 +73,11 @@ public bool isDir(string name)
 {
     try 
     {
-	return std.file.isDir(name);
+        return std.file.isDir(name);
     } 
     catch(Exception e)
     {
-	return false;
+        return false;
     }
 }
 
@@ -86,11 +85,11 @@ public bool isFile(string name)
 {
     try 
     {
-	return std.file.isFile(name);
+        return std.file.isFile(name);
     } 
     catch(Exception e)
     {
-	return false;
+        return false;
     }
 }
 
