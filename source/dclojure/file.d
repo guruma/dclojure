91a86cfb8139ec581b7eda80570cd5f66a793b08
Sang-Kyu Park <psk810@gmail.com>
2019-02-26 13:55:08 +0900

    Delete FSChar and cenforce

--- source/dclojure/file.d
+++ source/dclojure/file.d
@@ -9,107 +9,6 @@ import std.string,
 static import std.file;
 import std.stdio;
 
-version (Windows)
-{
-    import core.sys.windows.winbase, core.sys.windows.winnt, std.windows.syserror;
-}
-version (Windows)
-{
-    private alias FSChar = WCHAR;       // WCHAR can be aliased to wchar or wchar_t
-}
-else version (Posix)
-{
-    private alias FSChar = char;
-}
-else
-    static assert(0);
-
-private T cenforce(T)(T condition, lazy scope const(char)[] name, string file = __FILE__, size_t line = __LINE__)
-{
-    if (condition)
-        return condition;
-    version (Windows)
-    {
-        throw new std.file.FileException(name, .GetLastError(), file, line);
-    }
-    else version (Posix)
-    {
-        throw new std.file.FileException(name, .errno, file, line);
-    }
-}
-
-version (Windows)
-@trusted
-private T cenforce(T)(T condition, scope const(char)[] name, scope const(FSChar)* namez,
-    string file = __FILE__, size_t line = __LINE__)
-{
-    if (condition)
-        return condition;
-    if (!name)
-    {
-        import core.stdc.wchar_ : wcslen;
-        import std.conv : to;
-
-        auto len = namez ? wcslen(namez) : 0;
-        name = to!string(namez[0 .. len]);
-    }
-    throw new std.file.FileException(name, .GetLastError(), file, line);
-}
-
-version (Posix)
-@trusted
-private T cenforce(T)(T condition, scope const(char)[] name, scope const(FSChar)* namez,
-    string file = __FILE__, size_t line = __LINE__)
-{
-    if (condition)
-        return condition;
-    if (!name)
-    {
-        import core.stdc.string : strlen;
-
-        auto len = namez ? strlen(namez) : 0;
-        name = namez[0 .. len].idup;
-    }
-    throw new std.file.FileException(name, .errno, file, line);
-}
-
-/*
-bool isExec(string fname)
-{
-    bool isExist = false;
-
-    version (Windows)
-    {
-        import core.sys.windows.stat;
-        struct_stat st;
-
-	auto namez = fname.tempCString!FSChar();
-	static auto trustedStat(const(FSChar)* namez) @trusted
-	{
-	    return stat(namez, &st);
-	}
-
-        isExist = (0 == trustedStat(namez, &st));
-
-        if( ! isExist ) 
-            return false;
-
-        return (0 != (st.st_mode & S_IEXEC));
-    }
-    else version (Posix)
-    {
-        import core.sys.posix.sys.stat;
-        stat_t st;
-
-        isExist = (0 == stat(fname.toStringz, &st));
-
-        if( ! isExist ) 
-            return false;
-
-        return (0 != (st.st_mode & S_IXUSR));
-    }
-}
-*/
 
 private bool _isExec(R)(R name)
 if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
@@ -119,19 +18,13 @@ if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
     {
     	import core.sys.windows.stat;
 
-        static auto trustedStat(const(char)* namez, ref struct_stat st) 
+        static auto trustedStat(const(char)* namez, ref struct_stat st) @trusted
         {
 	        return stat(namez, &st);
         }
 
-	    struct_stat st = void;
+	struct_stat st = void;
         immutable result = trustedStat(name.toStringz(), st);
-
-        static if (isNarrowString!R && is(Unqual!(ElementEncodingType!R) == char))
-            alias names = name;
-        else
-            string names = null;
-        cenforce(result == 0, names);
 
         return (0 != (result & S_IEXEC));
 	//return (result & S_IFMT) == S_IEXEC;
@@ -139,34 +32,21 @@ if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
     else version (Posix)
     {
 	import core.sys.posix.sys.stat;
-        auto namez = name.tempCString!FSChar();
 
-        static auto trustedStat(const(FSChar)* namez, ref stat_t buf) @trusted
+        static auto trustedStat(const(char)* namez, ref stat_t buf) @trusted
         {
             return stat(namez, &buf);
         }
 
         stat_t st = void;
-        immutable result = trustedStat(namez, st);
-
-        static if (isNarrowString!R && is(Unqual!(ElementEncodingType!R) == char))
-            alias names = name;
-        else
-            string names = null;
-        cenforce(result == 0, names, namez);
-
-	writeln("names = ", names);
-	printf("st_mode = %x\n", st.st_mode);
+        immutable result = trustedStat(name.toStringz(), st);
 
         return (0 != (st.st_mode & S_IXUSR));
         //return (st.st_mode & S_IFMT) == S_IXUSR;
     }
 }
 
-
-public bool isExec(R)(R name)
-if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
-    !isConvertibleToString!R)
+public bool isExec(string name)
 {
     try 
     {
@@ -178,23 +58,7 @@ if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
     }
 }
 
-public bool isExec(R)(auto ref R name)
-if (isConvertibleToString!R)
-{
-    try 
-    {
-	return name._isExec!(StringTypeOf!R);
-    } 
-    catch(Exception e)
-    {
-	return false;
-    }
-}
-
-
-public bool exists(R)(R name)
-if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
-    !isConvertibleToString!R)
+public bool exists(string name)
 {
     try 
     {
@@ -206,22 +70,7 @@ if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
     }
 }
 
-public bool exists(R)(auto ref R name)
-if (isConvertibleToString!R)
-{
-    try 
-    {
-	return std.file.exists!(StringTypeOf!R)(name);
-    } 
-    catch(Exception e)
-    {
-	return false;
-    }
-}
-
-public bool isDir(R)(R name)
-if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
-    !isConvertibleToString!R)
+public bool isDir(string name)
 {
     try 
     {
@@ -233,39 +82,11 @@ if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
     }
 }
 
-public bool isDir(R)(auto ref R name)
-if (isConvertibleToString!R)
-{
-    try 
-    {
-	return std.file.isDir!(StringTypeOf!R)(name);
-    } 
-    catch(Exception e)
-    {
-	return false;
-    }
-}
-
-public bool isFile(R)(R name)
-if (isInputRange!R && !isInfinite!R && isSomeChar!(ElementEncodingType!R) &&
-    !isConvertibleToString!R)
+public bool isFile(string name)
 {
     try 
     {
 	return std.file.isFile(name);
-    } 
-    catch(Exception e)
-    {
-	return false;
-    }
-}
-
-public bool isFile(R)(auto ref R name)
-if (isConvertibleToString!R)
-{
-    try 
-    {
-	return std.file.isFile!(StringTypeOf!R)(name);
     } 
     catch(Exception e)
     {
